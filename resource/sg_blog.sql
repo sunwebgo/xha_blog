05744626b72609dfeb294a1801f1110d2fa2d9d7
xuhuaiang <107034518+sunwebgo@users.noreply.github.com>
2023-04-30 23:42:13 +0800

    Update sg_blog.sql

--- resource/sg_blog.sql
+++ resource/sg_blog.sql
@@ -45,24 +45,6 @@ CREATE TABLE `sg_article`
 -- ----------------------------
 -- Records of sg_article
 -- ----------------------------
-INSERT INTO `sg_article`
-VALUES (1, 'SpringSecurity从入门到精通',
-        '## 课程介绍\n![image20211219121555979.png](https://sg-blog-oss.oss-cn-beijing.aliyuncs.com/2022/01/31/e7131718e9e64faeaf3fe16404186eb4.png)\n\n## 0. 简介1\n\n​	**Spring Security** 是 Spring 家族中的一个安全管理框架。相比与另外一个安全框架**Shiro**，它提供了更丰富的功能，社区资源也比Shiro丰富。\n\n​	一般来说中大型的项目都是使用**SpringSecurity** 来做安全框架。小项目有Shiro的比较多，因为相比与SpringSecurity，Shiro的上手更加的简单。\n\n​	 一般Web应用的需要进行**认证**和**授权**。\n\n​		**认证：验证当前访问系统的是不是本系统的用户，并且要确认具体是哪个用户**\n\n​		**授权：经过认证后判断当前用户是否有权限进行某个操作**\n\n​	而认证和授权也是SpringSecurity作为安全框架的核心功能。\n\n\n\n## 1. 快速入门\n\n### 1.1 准备工作\n\n​	我们先要搭建一个简单的SpringBoot工程\n\n① 设置父工程 添加依赖\n\n~~~~\n    <parent>\n        <groupId>org.springframework.boot</groupId>\n        <artifactId>spring-boot-starter-parent</artifactId>\n        <version>2.5.0</version>\n    </parent>\n    <dependencies>\n        <dependency>\n            <groupId>org.springframework.boot</groupId>\n            <artifactId>spring-boot-starter-web</artifactId>\n        </dependency>\n        <dependency>\n            <groupId>org.projectlombok</groupId>\n            <artifactId>lombok</artifactId>\n            <optional>true</optional>\n        </dependency>\n    </dependencies>\n~~~~\n\n② 创建启动类\n\n~~~~\n@SpringBootApplication\npublic class SecurityApplication {\n\n    public static void main(String[] args) {\n        SpringApplication.run(SecurityApplication.class,args);\n    }\n}\n\n~~~~\n\n③ 创建Controller\n\n~~~~java\n\nimport org.springframework.web.bind.annotation.RequestMapping;\nimport org.springframework.web.bind.annotation.RestController;\n\n@RestController\npublic class HelloController {\n\n    @RequestMapping(\"/hello\")\n    public String hello(){\n        return \"hello\";\n    }\n}\n\n~~~~\n\n\n\n### 1.2 引入SpringSecurity\n\n​	在SpringBoot项目中使用SpringSecurity我们只需要引入依赖即可实现入门案例。\n\n~~~~xml\n        <dependency>\n            <groupId>org.springframework.boot</groupId>\n            <artifactId>spring-boot-starter-security</artifactId>\n        </dependency>\n~~~~\n\n​	引入依赖后我们在尝试去访问之前的接口就会自动跳转到一个SpringSecurity的默认登陆页面，默认用户名是user,密码会输出在控制台。\n\n​	必须登陆之后才能对接口进行访问。\n\n\n\n## 2. 认证\n\n### 2.1 登陆校验流程\n![image20211215094003288.png](https://sg-blog-oss.oss-cn-beijing.aliyuncs.com/2022/01/31/414a87eeed344828b5b00ffa80178958.png)',
-        'SpringSecurity框架教程-Spring Security+JWT实现项目级前端分离认证授权', 1,
-        'https://sg-blog-oss.oss-cn-beijing.aliyuncs.com/2022/01/31/948597e164614902ab1662ba8452e106.png', '1', '0',
-        134, '0', NULL, '2022-01-22 23:20:11', NULL, '2022-11-15 16:33:11', 0);
-INSERT INTO `sg_article`
-VALUES (5, 'Docker详解',
-        '# 1.docker为什么会出现\n\n一款产品从开发到上线，一般都会有`开发环境，测试环境，运行环境`。\n\n==如果有一个环境中某个软件或者依赖版本不同了，可能产品就会出现一些错误，甚至无法运行==。比如开发人员在windows系统，但是最终要把项目部署到linux。如果存在不支持跨平台的软件，那项目肯定也无法部署成功。\n\n这就产生了开发和运维人员之间的矛盾。开发人员在开发环境将代码跑通，但是到了上线的时候就崩了。还要重新检查操作系统，软件，依赖等版本，这大大降低了效率。造成了搭环境一两天，部署项目两分钟的事件。\n\ndocker的出现就能解决以上问题：\n\n开发人员把环境配置好，将需要运行的程序包运行成功，然后把==程序包和环境==一起打包给运维人员，让运维人员部署就可以了。这大大提高了项目上线的效率。\n\n\n\n# 2.docker简介\n\nDocker是基于Go语言实现的云开源项目。\n\nDocker的主要目标是`“Build，Ship and Run Any App,Anywhere”`，==也就是通过对应用组件的封装、分发、部署、运行等生命周期的管理，使用户的APP（可以是一个WEB应用或数据库应用等等）及其运行环境能够做到“<font color=\'red\'>一次镜像，处处运行</font>”==\n\n![image.png](https://imagebed-xuhuaiang.oss-cn-shanghai.aliyuncs.com/typora/b3ffe73fea1d51a52849159c521b3e84.png)\n\nLinux容器技术的出现就解决了这样一个问题，而 Docker 就是在它的基础上发展过来的。<font color=\'red\'>将应用打成镜像，通过镜像成为运行在Docker容器上面的实例，而 Docker容器在任何操作系统上都是一致的，这就实现了跨平台、跨服务器。只需要一次配置好环境，换到别的机器上就可以一键部署好，大大简化了操作</font>。\n\ndocker简介总结：\n\n==解决了运行环境和配置问题的软件容器，方便做持续集成并有助于整体发布的容器虚拟化技术。docker基于Linux内核，仅包含业务运行所需的runtime环境。==',
-        'Docker详解', 17,
-        'https://imagebed-xuhuaiang.oss-cn-shanghai.aliyuncs.com/sangengblog/63ead20b51854d2e898f27a0568ad5af.jpeg',
-        '1', '0', 44, '0', NULL, '2022-01-17 14:58:37', NULL, '2022-11-15 16:32:52', 0);
-INSERT INTO `sg_article`
-VALUES (8, 'Redis从入门到高级',
-        'Redis是一个NoSQL数据库，其数据都在内存中，支持持久化，主要用作备份恢复。<font color=\'cornflowerblue\'>除了支持简单的key-value模式，还支持多种数据结构的存储，比如 list、set、hash、zset等。</font>==一般是作为缓存数据库辅助持久化的数据库==\n![image-20220921122545305](https://imagebed-xuhuaiang.oss-cn-shanghai.aliyuncs.com/typora/image-20220921122545305.png)\nredis诞生小故事：\n\n说起我的诞生，跟关系数据库MySQL还挺有渊源的。\n\n在我还没来到这个世界上的时候，MySQL过的很辛苦，互联网发展的越来越快，<font color=\'red\'>它容纳的数据也越来越多，用户请求也随之暴涨</font>，而每一个用户请求都变成了对它的一个又一个读写操作，MySQL是苦不堪言。尤其是到“双11”、“618“这种全民购物狂欢的日子，都是MySQL受苦受难的日子。\n\n<font color=\'red\'>据后来MySQL告诉我说，其实有一大半的用户请求都是读操作，而且经常都是重复查询一个东西，浪费它很多时间去进行磁盘I/O。</font>\n\n后来有人就琢磨，是不是可以学学CPU，给数据库也加一个缓存呢？于是我就诞生了！\n\n出生不久，我就和MySQL成为了好朋友，我们俩常常携手出现在后端服务器中。\n\n<font color=\'cornflowerblue\'>应用程序们从MySQL查询到的数据，在我这里登记一下，后面再需要用到的时候，就先找我要，我这里没有再找MySQL要。</font>\n![image-20220921122636427](https://imagebed-xuhuaiang.oss-cn-shanghai.aliyuncs.com/typora/image-20220921122636427.png)',
-        'Redis从入门到高级', 15,
-        'https://imagebed-xuhuaiang.oss-cn-shanghai.aliyuncs.com/sangengblog/205af0ea438f4af6adca179086b7a7d1.png', '0',
-        '0', 0, '0', 1, '2022-11-13 11:15:01', NULL, '2022-11-13 16:37:21', 0);
 
 -- ----------------------------
 -- Table structure for sg_article_tag
@@ -194,13 +176,6 @@ CREATE TABLE `sg_link`
 -- ----------------------------
 -- Records of sg_link
 -- ----------------------------
-INSERT INTO `sg_link`
-VALUES (4, '哔哩哔哩', 'https://imagebed-xuhuaiang.oss-cn-shanghai.aliyuncs.com/sangengblog/bilibili.jpg',
-        '哔哩哔哩（bilibili.com)是国内知名的视频弹幕网站', 'www.bilibili.com', '0', 1, '2022-11-12 19:22:41', NULL,
-        '2022-11-12 22:21:25', 0);
-INSERT INTO `sg_link`
-VALUES (7, 'IMusic音乐', 'https://imagebed-xuhuaiang.oss-cn-shanghai.aliyuncs.com/sangengblog/wangyi.jpg',
-        '基于SpringBoot+Vue的音乐网站', '43.143.117.57:8092', '0', 1, '2022-11-12 19:40:16', NULL, '2022-11-12 21:13:49', 0);
 
 -- ----------------------------
 -- Table structure for sg_tag
